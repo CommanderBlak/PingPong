1eca2e9407c1da01a0423852b4df6d7706aad9b7
CommanderBlak <mwandiwayne@gmail.com>
2017-06-09 14:41:31 +0300

    adds final files and directories

---
+++ README.md
@@ -0,0 +1,16 @@
+Author: **Wayne Mwandi** 
+GitHub handle: **CommanderBlak**
+
+# Ping Pong
+### This program allows a user to enter a number and returns a range of numbers from 1 to the chosen number with the following exceptions:
+* Numbers divisible by 3 are replaced with "ping".
+* Numbers divisible by 5 are replaced with "pong".
+* Numbers divisible by 15 are replaced with "pingpong".
+## Setup
+* Install by cloning this repository.
+* Open index.html in a browser 
+##### OR
+* Click on this [link](https://CommanderBlack.github.io/PingPong) to access the gh-pages.
+## Legal
+Copyright(c)2017 **Wayne Mwandi**
+This program is licensed under the MIT license.
---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+var output = function(input) {
+  for (var i = 1; i <= input; i++) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+      $("#list").append('<li>' + "pingpong" + '</li>')
+      //outputArray.push("pingpong");
+    } else if (i % 3 === 0) {
+      $("#list").append('<li>' + "ping" + '</li>')
+      //outputArray.push("ping");
+    } else if (i % 5 === 0) {
+      $("#list").append('<li>' + "pong" + '</li>')
+      //outputArray.push("pong");
+    } else {
+      $("#list").append('<li>' + i + '</li>')
+
+      //outputArray.push(i);
+    }
+  };
+};
+
+
+$(document).ready(function() {
+  $(".form").submit(function(event) {
+    event.preventDefault();
+    var input = parseInt($("#userInput").val());
+
+    var result = output(input);
+
+    //$(".list2").text(outputArray);
+  })
+});
